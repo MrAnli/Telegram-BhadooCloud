756b120b3086532912eb03ff59d6d2ce7aefd6a2
MrAnli <66670206+MrAnli@users.noreply.github.com>
2020-06-09 22:41:10 -0400

    Update .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-  TOKEN: '1262179045:AAGvMF_SYhk6TyxxGemDsoCIQQqm7doErCQ', //required
+  TOKEN: '1146290375:AAGmH0cnlpHxBrKhUulGb3UU4mNVBvLxtHY', //Token Telegram Bots
   ARIA_SECRET: 'bhadoo', //change as per needs.
   ARIA_DOWNLOAD_LOCATION: '/app/downloads)', //-leave as it is
   INDEX_DOMAIN: 'https://workers.workers.dev/', // replace with your index domain. use https://github.com/ParveenBhadooOfficial/go-drive
@@ -7,7 +7,7 @@ module.exports = Object.freeze({
   ARIA_FILTERED_DOMAINS: [], // Prevent downloading from URLs containing these substrings -leave as it is
   ARIA_FILTERED_FILENAMES: [], // Files/top level directories with these substrings in the filename won't be downloaded -leave as it is
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well -leave as it is
-  GDRIVE_PARENT_DIR_ID:  '1e7Wlk6aNwODPQ1155aFleA3yg6aRXdDY', // for directorty eg. https://drive.google.com/drive/folders/1p7pHhSh2yboWePmo53DnVr3xkAYiojP9 use 1p7pHhSh2yboWePmo53DnVr3xkAYiojP9
+  GDRIVE_PARENT_DIR_ID:  '1m5vgC6TnpnXAP4F6NPdSiCRyJM5hpOvT', // for directorty eg. https://drive.google.com/drive/folders/1p7pHhSh2yboWePmo53DnVr3xkAYiojP9 use 1p7pHhSh2yboWePmo53DnVr3xkAYiojP9
   SUDO_USERS: [1182495204],	// Telegram user IDs. These users can use the bot in any chat. required, get it using https://t.me/YourTGid_Bot
   AUTHORIZED_CHATS: [-1182495204],	// Telegram chat IDs. Anyone in these chats can use the bot. Add https://t.me/YourTGid_Bot to Group and click /id
   STATUS_UPDATE_INTERVAL_MS: 12000, // A smaller number will update faster, but might cause rate limiting -leave as it is
@@ -23,7 +23,7 @@ module.exports = Object.freeze({
   },
     COMMANDS_USE_BOT_NAME: {
       ENABLED: true,  // If true, all commands except '/list' has to have the bot username after the command.
-      NAME: "@AnliUploader_Bot" //Enter your Bot Username here if above is set to true.
+      NAME: "@TukangUpload_Bot" //Enter your Bot Username here if above is set to true.
     }
 });
 
